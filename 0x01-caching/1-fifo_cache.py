df5def3418587a1a14a39645f0f87c4f300d4e68
KhotsoSelading <k.selading007@gmail.com>
2024-01-30 17:13:13 +0200

    Task Complete - Khotso

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -27,12 +27,12 @@ class FIFOCache(BaseCaching):
                 return key
 
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                item_discarded = self.key_indexes.pop(0)
+                item_discarded = self.order.pop(0)
                 del self.cache_data[item_discarded]
                 print("DISCARD:", item_discarded)
 
             self.cache_data[key] = item
-            self.key_indexes.append(key)
+            self.order.append(key)
 
     def get(self, key):
         """
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -25,14 +25,14 @@ class LIFOCache(BaseCaching):
             if len(self.cache_data) >= self.MAX_ITEMS:
                 if key in self.cache_data:
                     del self.cache_data[key]
-                    self.key_indexes.remove(key)
+                    self.order.remove(key)
                 else:
-                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
-                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    del self.cache_data[self.order[self.MAX_ITEMS - 1]]
+                    item_discarded = self.order.pop(self.MAX_ITEMS - 1)
                     print("DISCARD:", item_discarded)
 
             self.cache_data[key] = item
-            self.key_indexes.append(key)
+            self.order.append(key)
 
     def get(self, key):
         """
