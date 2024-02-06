54d7b3e987ce61c873663316885bb4e45e52fb8c
KhotsoSelading <k.selading007@gmail.com>
2024-02-06 17:13:10 +0200

    Task 4 - Khotso

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -4,57 +4,45 @@ This script is related to the i18n topic.
 Author: Khotso Selading
 Date: 06-02-2024
 """
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+import babel
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config(object):
-    """
-    Configuration settings for Babel.
-
-    Attributes:
-        LANGUAGES (list): List of supported languages.
-        BABEL_DEFAULT_LOCALE (str): Default locale for Babel.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone for Babel.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
+
+
+class Config:
+    """
+    Configuration settings for Flask Babel.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
     """
-    Selects and returns the best language match based on supported languages.
-
-    Returns:
-        str: The selected language code.
+    Detects if the incoming request contains a 'locale' argument and if its
+    value is a supported locale, returns it.
     """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
+    locale = request.args.get('locale')
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
     """
-    Renders the index page.
-
-    Returns:
-        str: Rendered HTML content for the index page.
+    Renders a basic 'Hello, World!' page.
     """
     return render_template('4-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
